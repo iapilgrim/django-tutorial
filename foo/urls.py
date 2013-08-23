4597574e84b0d1b9cad2c5b7ae21fb75d8350afa
Erik Johnson <palehose@gmail.com>
2013-08-22 20:30:37 -0600

    polls.urls -> foo.polls.urls

--- foo/urls.py
+++ foo/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('foo.polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
