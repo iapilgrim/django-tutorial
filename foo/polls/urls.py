386b21f29f32dfdbffcedf77eec4dbb3f65428f7
Seth House <seth@eseth.com>
2013-08-31 02:00:52 -0600

    Named the poll list and poll detail views

--- foo/polls/urls.py
+++ foo/polls/urls.py
@@ -2,17 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from foo.polls.models import Poll
 
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns('',
     url(r'^$',
         ListView.as_view(
             queryset=Poll.objects.order_by('-pub_date')[:5],
             context_object_name='latest_poll_list',
-            template_name='polls/index.html')),
+            template_name='polls/index.html'), name='poll_list'),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Poll,
-            template_name='polls/detail.html')),
+            template_name='polls/detail.html'), name='poll_detail'),
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
             model=Poll,
